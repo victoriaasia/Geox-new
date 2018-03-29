1d5080c346fbb53aa52b841f88c5b4996492ec96
Королева Виктория Игоревна <Koroleva.VI@esphere.local>
2018-03-29 12:37:32 +0300

    f

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,34 @@
-$(function() {
-
-var $socials  = $(".js-g-social"),
-    $window    = $(window),
-    offset     = $socials.offset(),
-    topPadding = 100;
-
-
-$(window).resize(function() {
-  if ($(window).width() > 768) {
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $socials.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $socials.stop().animate({
-                marginTop: 0
-            });
-        }
-    });
-   }
-  else {
-    $window.scroll(function() {
-      if ($window.scrollTop() > offset.top) {
-        $socials.stop().animate({
-            marginTop: 0
-        });
-      }
-    });
-  }
-});
-
-});
+// $(function() {
+//
+// var $socials  = $(".js-g-social"),
+//     $window    = $(window),
+//     offset     = $socials.offset();
+//     // topPadding = 100;
+//
+//
+// $(window).resize(function() {
+//   if ($(window).width() > 768) {
+//     $window.scroll(function() {
+//         if ($window.scrollTop() > offset.top) {
+//             $socials.stop().animate({
+//                 marginTop: $window.scrollTop() - offset.top
+//             });
+//         } else {
+//             $socials.stop().animate({
+//                 marginTop: 0
+//             });
+//         }
+//     });
+//    }
+//   else {
+//     $window.scroll(function() {
+//       if ($window.scrollTop() > offset.top) {
+//         $socials.stop().animate({
+//             marginTop: 0
+//         });
+//       }
+//     });
+//   }
+// });
+//
+// });
